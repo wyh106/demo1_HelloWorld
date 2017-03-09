61fb4a23f695044a50559b656d8abf67bb106e06
WYH106 <yuanhang.wang@mljr.com>
2017-03-09 11:39:12 +0800

    （14）Spring Boot定时任务的使用

---
+++ src/main/java/com/app/config/scheduling/SchedulingConfig.java
@@ -0,0 +1,18 @@
+package com.app.config.scheduling;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Created by lenovo on 2017/3/9.
+ * 定时任务
+ */
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+    @Scheduled(cron="0/5 * * * * ?")
+    public void scheduler(){
+        System.out.println(">>>>>>>>>>>>> SchedulingConfiguration.scheduler()");
+    }
+}
